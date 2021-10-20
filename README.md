612911f2e08038907a6c855f70d5a293e69ed422
TheTimidShade <thetimidshade@gmail.com>
2021-10-20 20:21:25 +1030

    Added stringtable support
    - Added stringtable support.

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Source code for my AIO pack.
 ## Changelog
 Read below for complete changelog history.
 
+### 20/10/2021
+- Added stringtable support.
+
 ### 10/10/2021
 - Added 'Configure Mods' module to enable/disable each TTS mod.
 
--- tts_effects_aio/cfgVehicles.hpp
+++ tts_effects_aio/cfgVehicles.hpp
@@ -31,7 +31,7 @@ class CfgVehicles {
 		isDisposable = 1; // 1 if modules is to be disabled once it's activated (i.e., repeated trigger activation won't work)
 		isGlobal = 0; // 0 for server only execution, 1 for global execution, 2 for persistent global execution
 		isTriggerActivated = 0;
-		displayName = "Configure Mods";
+		displayName = "$STR_tts_effects_aio_moduleConfigureMods_title";
 		icon = "tts_effects_aio\icons\tts_settings.paa";
 		category = "tts_effects_aio_modules";
 		function = "tts_effects_aio_fnc_moduleConfigureMods";
@@ -40,19 +40,19 @@ class CfgVehicles {
 			class EnableBeam : Checkbox {
 				property = "tts_effects_aio_moduleConfigureMods_EnableBeam";
 				displayName = "TTS Beam Laser";
-				tooltip = "If unchecked, TTS Beam Laser will be disabled.\n(ZEN modules will be hidden)";
+				tooltip = "$STR_tts_effects_aio_moduleConfigureMods_enableBeam_desc";
 				defaultValue = "1";
 			};
 			class EnableCloak : Checkbox {
 				property = "tts_effects_aio_moduleConfigureMods_EnableCloak";
 				displayName = "TTS Cloak/Active Camo";
-				tooltip = "If unchecked, TTS Cloak/Active Camo will be disabled.\n(Some scripts will be disabled and ZEN modules will be hidden)";
+				tooltip = "$STR_tts_effects_aio_moduleConfigureMods_enableCloak_desc";
 				defaultValue = "1";
 			};
 			class EnableEmission : Checkbox {
 				property = "tts_effects_aio_moduleConfigureMods_EnableEmission";
 				displayName = "TTS Emission (Blowout)";
-				tooltip = "If unchecked, TTS Emission (Blowout) will be disabled.\n(ZEN modules will be hidden)";
+				tooltip = "$STR_tts_effects_aio_moduleConfigureMods_enableEmission_desc";
 				defaultValue = "1";
 			};
 			class ModuleDescription: ModuleDescription{};
@@ -60,7 +60,7 @@ class CfgVehicles {
 
 		class ModuleDescription: ModuleDescription
 		{
-			description = "Controls which TTS Effects mods are enabled. Mods can be disabled individually to clear clutter in Zeus interface and prevent scripts from running when not being used.<br/>For some mods this will simply hide the ZEN modules but for others it may disable scripts from firing. Read the tooltip for each checkbox to see what it will enable/disable."; // Short description, will be formatted as structured text
+			description = "$STR_tts_effects_aio_moduleConfigureMods_desc"; // Short description, will be formatted as structured text
 		};
 	};
 }
